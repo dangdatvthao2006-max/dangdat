690ab0ef4b0485a27c43cfe2e1abbe5a72b5abf3
dangdatvthao2006-max <dangdatvthao2006@gmail.com>
2025-12-03 09:42:15 +0700

    Update c2_2.py

--- cau2/c2_2.py
+++ cau2/c2_2.py
@@ -1,4 +1,4 @@
-print("Sinh viên: Nguyễn Dang Dat")
+print("Sinh viên: Nguyen Dang Dat")
 print("MSV: 245752021610124")
 
 import math
@@ -14,4 +14,5 @@ d2 = (y2 - y1) * (y2 - y1)
 res = math.sqrt(d1 + d2)
 
 print("Distance between two points:", res)
+
 
